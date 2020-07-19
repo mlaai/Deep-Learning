a3776e6a40f466eb0e7b0be7f4502a9a76400e4b
Anurag <54783887+mlaai@users.noreply.github.com>
2020-07-18 22:51:58 -0400

    Added Model Convolution Neural Network

--- Code/Projects/TensorFlow/trainCallback.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf 
-
-class trainCallback(tf.keras.callbacks.Callback):
-    def on_epoch_end(self, epoch, percent, logs={}):
-        if(logs.get('acc') > percent/100):
-            print("\nReached {percent} % accuracy so cancelling training!")
-            self.model.stop_training = True
